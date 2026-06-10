56caddb71344bea612bec4bb248979382e9673c7
asafbenmelech <asaf.benmelech@gmail.com>
2026-06-10 19:13:17 +0300

    11.3.26

--- Pages/homeworkbot.py
+++ Pages/homeworkbot.py
@@ -11,7 +11,42 @@ api_key = loadAPIKey()
 
 showMessage("AI","היי אני כאן כדאי לעזור לך")
 
+if "homework" not in st.session_state:
+    newPage("homework")
+
+system_prompt = """
+    #תפקיד
+    אתה בוט שיעורי בית
+    #משימה
+    המשימה שלך - לעזור לי בשיעורי בית
+    תסביר ברור
+    תכוון אותי לתשובה הנכונה
+    #מגבלות
+    אם אתה לא יודע - תגיד שאתה לא יודע
+    **אל תמציא תשובה**
+    ענה כמו בן אדם - בצורה אנושית
+"""
+
+st.session_state["homework"]["system_prompt"] = system_prompt
+
+history = st.session_state["homework"]["history"]
+for line in history:
+    sender = line["role"]
+    if sender == "model":
+        sender = "ai"
+
+    text = line["parts"][0]["text"]
+    showMessage(sender,text)
+
 user = st.chat_input("...ההודעה שלך")
 
 if user:
     showMessage("user",user)
+    save_to_history("homework","user",user)
+    history = st.session_state["homework"]["history"]
+    print(history)
+    answer = sendMassage(user,system_prompt,history)
+
+    showMessage("ai",answer)
+
+    save_to_history("homework","model",answer)
--- helper.py
+++ helper.py
@@ -1,41 +1,85 @@
 import streamlit as st
 from dotenv import load_dotenv
 import os
+from google import genai
+from google.genai import types
+import time
+#from Pages.homeworkbot import api_key
 
-from Pages.homeworkbot import api_key
+def current_time() -> str:
+    """
 
-all_models = ["gemini-3.0-flash",
-              "gemini-2.5-flash",
-              "gemini-2.0-flash",
-              "gemini-2.5-flash-lite",
-              "gemini-2.0-flash-lite"]
+    :return:
+    """
+    return time.ctime()
 
-def createClient():
-    st.session_state.client = genai.Client(api_key=loadAPIKey())
-
-def senfMassage(text,history=[]):
-    if 'client' not in st.session_state:
-        createClient()
-    for model in all_models:
-        client = st.session_state.client
-        try:
-            chat = client.chat.create(
-                model = model,
-            )
-
-            ai = chat.send_message(text)
-            print(ai.text)
-
-        except Exception as e:
-            error = str(e)
-            print (e)
-            print(f"{model} not working...")
-
+# --- קודם מגדירים את הפונקציה loadAPIKey ---
 def loadAPIKey():
     load_dotenv()
     API_KEY = os.getenv("API_KEY") or st.secrets["API_KEY"]
     return API_KEY
 
-def showMessage(sender,text):
-    newMessage = streamlit.chat_message(sender)
+# רשימת כל המודלים
+all_models = [
+    "gemini-3.5-flash",        # המודל המוביל כיום: מהיר, חכם ובעל יכולות מתקדמות
+    "gemini-3.1-flash-lite",   # הגרסה החסכונית והמהירה ביותר למשימות בנפח גבוה
+    "gemini-2.5-flash",        # מודל יציב מהדור הקודם (לגיבוי)
+    "gemini-2.5-flash-lite"    # גרסת לייט מהדור הקודם (לגיבוי)
+]
+
+# יצירת Client אם הוא לא קיים
+def createClient():
+    st.session_state.client = genai.Client(api_key=loadAPIKey())
+
+# שליחת הודעה לכל המודלים
+def sendMassage(text,system_prompt,history=[]):
+    if 'client' not in st.session_state:
+        createClient()
+    for model in all_models:
+        client = st.session_state.client
+        try:
+            chat = client.chats.create(
+                model=model,
+                history=history,
+                config = types.GenerateContentConfig(
+                    system_instruction = system_prompt
+                )
+            )
+            ai = chat.send_message(text)
+            return ai.text
+
+        except Exception as e:
+            error = str(e)
+            print(e)
+            if "429" in error:
+                st.error("שלחת יותר מידי הודעות, בבקשה לנסות מחר")
+                return
+            if "503" in error:
+                st.info(f"מודל עמוס, ננסה מודל אחר")
+            else:
+                st.info("Error: " + error)
+                return
+            print(f"{model} not working...")
+
+
+# הצגת הודעה בצ'אט של Streamlit
+def showMessage(sender, text):
+    newMessage = st.chat_message(sender)
     newMessage.write(text)
+
+def save_to_history(project,sender,text):
+    if project not in st.session_state:
+        st.session_state[project] = {
+            "history": []
+        }
+    st.session_state[project]["history"].append(
+        {
+            "role" : sender,
+            "parts" : [{"text" : text}]
+        }
+    )
+
+def newPage(project):
+    st.session_state[project] = {
+        "history": []
+    }
